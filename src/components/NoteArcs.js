2e8ef694974609fcebbdd3c5910fdfb0000ad4c3
David Mora <davidnmora@gmail.com>
2021-12-17 21:25:48 -0800

    Add arcs for 1 note in all keys

--- src/components/NoteArcs.js
+++ src/components/NoteArcs.js
@@ -1,27 +1,27 @@
 import { scaleBand } from 'd3-scale'
 import { arc as d3arc } from 'd3-shape'
 import { A_FULL_KEY_ANGLE } from '../use-derived-state'
-import { CANVAS_HEIGHT, CANVAS_WIDTH } from './CirlceOfFifthsViz'
+import {
+  CANVAS_HEIGHT,
+  CANVAS_WIDTH,
+  CIRCLE_NOTES_DATA,
+} from './CirlceOfFifthsViz'
 const MIN_RADIUS = 20
 const MAX_RADIUS = 130
 
 const a4 = 69
 const a5 = 81
 
-const getNoteStartAndEndAngles = () => {
-  const startAngle = 0
-  const endAngle = 0 + A_FULL_KEY_ANGLE
+const getNoteStartAndEndAngles = (noteIndex) => {
+  const startAngle = noteIndex * A_FULL_KEY_ANGLE
+  const endAngle = startAngle + A_FULL_KEY_ANGLE - 0.05
   return { startAngle, endAngle }
 }
 const arcGenerator = d3arc()
 
-const NoteArcForAKey = (noteNum) => {
-  const { startAngle, endAngle } = getNoteStartAndEndAngles()
-  const noKeysMatch = [startAngle, endAngle].includes(undefined)
+const NoteArcForAKey = ({ noteIndex }) => {
+  const { startAngle, endAngle } = getNoteStartAndEndAngles(noteIndex)
 
-  if (noKeysMatch) {
-    return null
-  }
   const arcD = arcGenerator({
     innerRadius: 20,
     outerRadius: 40,
@@ -37,7 +37,9 @@ const NoteArcForAKey = (noteNum) => {
 }
 
 const NoteArcsForASingleNote = () => {
-  return ['blah'].map((noteInAKey) => <NoteArcForAKey />)
+  return CIRCLE_NOTES_DATA.map((_, noteIndex) => (
+    <NoteArcForAKey key={noteIndex} noteIndex={noteIndex} />
+  ))
 }
 
 const NoteArcs = () => {
