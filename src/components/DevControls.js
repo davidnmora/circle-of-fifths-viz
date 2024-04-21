95cdf4deb6c9ffb1cf64643aa9e9eb701a27fbd9
David Mora <davidnmora@gmail.com>
2024-04-21 10:49:16 -0700

    Add sample bass note buttons

--- src/components/DevControls.js
+++ src/components/DevControls.js
@@ -30,15 +30,30 @@ const SAMPLE_TREBLE_NOTES = [
     { noteName: 'G', octave: 5, noteNum: 79 },
   ],
 ]
+
+const SAMPLE_BASS_NOTES = [
+  { noteName: 'F', octave: 3, noteNum: 53 },
+  { noteName: 'G', octave: 3, noteNum: 55 },
+  { noteName: 'A', octave: 3, noteNum: 57 },
+  { noteName: 'Bb', octave: 3, noteNum: 58 },
+  { noteName: 'B', octave: 3, noteNum: 59 },
+  { noteName: 'C', octave: 4, noteNum: 60 },
+  { noteName: 'Db', octave: 4, noteNum: 61 },
+  { noteName: 'D', octave: 4, noteNum: 62 },
+  { noteName: 'Eb', octave: 4, noteNum: 63 },
+  { noteName: 'E', octave: 4, noteNum: 64 },
+]
+
 const DevControls = () => {
   const updateInputState = useUpdateInputState()
   const [trebleIndex, setTrebledIndex] = useState(null)
+  const [bassIndex, setBassIndex] = useState(null)
   return (
     <div>
       <SubTitle>Select Treble notes:</SubTitle>
       {SAMPLE_TREBLE_NOTES.map((trebleNotes, i) => (
         <button
-          key={trebleNotes.toString()}
+          key={i}
           onClick={() => {
             setTrebledIndex(i)
             updateInputState((draft) => {
@@ -50,6 +65,22 @@ const DevControls = () => {
           {`Make note ${trebleNotes
             .map(({ noteName }) => noteName)
             .toString()}`}
+        </button>
+      ))}
+
+      <SubTitle>Select Bass note:</SubTitle>
+      {SAMPLE_BASS_NOTES.map((bassNote, i) => (
+        <button
+          key={bassNote.noteName}
+          onClick={() => {
+            setBassIndex(i)
+            updateInputState((draft) => {
+              draft.bassNote = bassNote
+            })
+          }}
+          style={{ opacity: bassIndex === i ? 1 : 0.7 }}
+        >
+          {`Make note ${bassNote.noteName}`}
         </button>
       ))}
     </div>
